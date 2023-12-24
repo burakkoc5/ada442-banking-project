4562fa3097819007bab36c532a1c033924807146
Burak Koç <74610612+burakkoc5@users.noreply.github.com>
2023-12-24 16:05:02 +0300

    v2.0.1

--- main.py
+++ main.py
@@ -79,7 +79,10 @@ def button_onclick(input_model, user_data):
 
     # Display the prediction
     st.subheader("Prediction")
-    st.write(f"<span style='font-size:24px'>{result}</span>", unsafe_allow_html=True)
+    if result == 'Yes':
+        st.write(f"<span style='font-size:20px; color:green'>{result}</span>", unsafe_allow_html=True)
+    else:
+        st.write(f"<span style='font-size:20px; color:red'>{result}</span>", unsafe_allow_html=True)
 
 
 if __name__ == '__main__':
